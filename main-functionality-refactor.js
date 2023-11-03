a8736012ad40e261f2dd6fe9fb72106c3588f153
alexduffdesign <alexduffblog@gmail.com>
2023-11-03 10:36:27 +0000

    Update main-functionality-refactor.js

--- main-functionality-refactor.js
+++ main-functionality-refactor.js
@@ -431,6 +431,7 @@ document.addEventListener("DOMContentLoaded", function () {
         button.addEventListener('click', function(event) {
           const stepToRemove = parseInt(event.currentTarget.dataset.step, 10);
           const bundleProduct = event.currentTarget.closest("[bundle-item]");
+          console.log("bundle product identifyed", bundleProduct);
           removeProduct(stepToRemove, bundleProduct);
         })
       });
