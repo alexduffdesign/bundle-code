2a434cf18de7836f502fa3f736e2db4f9db14e0b
alexduffdesign <alexduffblog@gmail.com>
2023-11-06 18:40:16 +0000

    Update main-functionality-refactor.js

--- main-functionality-refactor.js
+++ main-functionality-refactor.js
@@ -260,13 +260,9 @@ document.addEventListener("DOMContentLoaded", function () {
     
       // Set editingStep to the chosen step
       state.editingStep = stepValue;
-          
-      // Calculate the zero-based index for JS array access
-      const stepIndex = state.editingStep - 1;
-      console.log("Step Index", stepIndex);
 
       // Update the UI to reflect the editing mode state
-      updateUIForEditMode(stepIndex);
+      updateUIForEditMode(state.editingStep);
     }
 
     function removeProduct(stepToRemove, bundleProduct) { 
