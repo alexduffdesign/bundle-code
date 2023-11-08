1188c54634a617bb0c0bf243cd130b9080cde2e2
alexduffdesign <alexduffblog@gmail.com>
2023-11-08 19:20:27 +0000

    Update main-functionality-refactor.js

--- main-functionality-refactor.js
+++ main-functionality-refactor.js
@@ -597,7 +597,7 @@ document.addEventListener("DOMContentLoaded", function () {
         }
       } else {
         // Add a new step with a new product ids array
-        bundle.push({ step, productIds: [idNumber], price: price });
+        bundle.push({ step, productIds: [idNumber, price] });
       }
       
       return bundle; // Return the new state of the bundle
